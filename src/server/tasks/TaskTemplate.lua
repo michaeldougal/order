e95b1337edaa43cfe708fa51d9071f74b845fe04
Michael Dougal <michaeldougal@yahoo.com>
2022-09-13 22:50:47 -0600

    Added templates and empty folder keepers

---
+++ src/server/tasks/TaskTemplate.lua
@@ -0,0 +1,40 @@
+--[[
+	Author: ChiefWildin
+	Module: TaskTemplate.lua
+	Created: 09/13/2022
+--]]
+
+-- Services
+
+
+-- Module Declaration
+
+local TaskTemplate = {}
+
+
+-- Dependencies
+
+
+-- Constants
+
+
+-- Global Variables
+
+
+-- Objects
+
+
+-- Private Functions
+
+
+-- Public Functions
+
+
+-- Task Initialization
+
+function TaskTemplate:Init()
+
+end
+
+
+return TaskTemplate
